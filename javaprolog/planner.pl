a04b5abc63788d345e5948df5bfda9236cba74ea
boutry <boutry@sushis.u-strasbg.fr>
2014-05-17 02:22:56 +0200

    more split

--- javaprolog/planner.pl
+++ javaprolog/planner.pl
@@ -1,5 +1,6 @@
 :- [canbeon].
 :- [retrievegoal].
+:- [planner_tools].
 :- style_check(-singleton).
 
 %Take the selected object if the arm does not hold something
@@ -508,26 +509,3 @@ plan(_Goal, World, _, _Objects, Plan) :-
 		%or not
 		; Plan = [[[],what]]
 	).
-
-%%--------------------------------------------------------------
-
-%tests if element is the head of the list
-checkHead([H|T],Element) :- H = Element.
-
-%return the number K if X is in the Kth list of lists LL
-%findall(X,whichListInTheWorld(a,[[d,e,f],[a,b,c]],X),R).
-
-whichListInTheWorld([L|_],X,0) :- member(X,L).
-whichListInTheWorld([_|LL],X,N) :- whichListInTheWorld(LL,X,M), N is M + 1.
-
-%the third argument is the list of lists corresponding to the one given as second argument in which the head is removed in the list of number: first argument
-pickAt(0,[[H|T1]|T2],[T1|T2]).
-pickAt(N,[H|T1],[H|T2]) :- pickAt(M,T1,T2), N is M + 1.
-
-%the third argument is the list of lists corresponding to the one given as second argument in which the first argument is added at the head in the list of number: second argument
-dropAt(Element,0,[T1|T2],[[Element|T1]|T2]).
-dropAt(Element,N,[H|T1],[H|T2]) :- dropAt(Element,M,T1,T2), N is M + 1.
-
-%find a list in which an element can be added given the object, the world, the objects
-canbeAt(X,[H|L],Objects,0) :- canbeon(X,H,Objects).
-canbeAt(X,[H|L],Objects,N) :- canbeAt(X,L,Objects,M), N is M + 1.
---
+++ javaprolog/planner_tools.pl
@@ -0,0 +1,22 @@
+:- style_check(-singleton).
+
+%tests if element is the head of the list
+checkHead([H|T],Element) :- H = Element.
+
+%return the number K if X is in the Kth list of lists LL
+%findall(X,whichListInTheWorld(a,[[d,e,f],[a,b,c]],X),R).
+
+whichListInTheWorld([L|_],X,0) :- member(X,L).
+whichListInTheWorld([_|LL],X,N) :- whichListInTheWorld(LL,X,M), N is M + 1.
+
+%the third argument is the list of lists corresponding to the one given as second argument in which the head is removed in the list of number: first argument
+pickAt(0,[[H|T1]|T2],[T1|T2]).
+pickAt(N,[H|T1],[H|T2]) :- pickAt(M,T1,T2), N is M + 1.
+
+%the third argument is the list of lists corresponding to the one given as second argument in which the first argument is added at the head in the list of number: second argument
+dropAt(Element,0,[T1|T2],[[Element|T1]|T2]).
+dropAt(Element,N,[H|T1],[H|T2]) :- dropAt(Element,M,T1,T2), N is M + 1.
+
+%find a list in which an element can be added given the object, the world, the objects
+canbeAt(X,[H|L],Objects,0) :- canbeon(X,H,Objects).
+canbeAt(X,[H|L],Objects,N) :- canbeAt(X,L,Objects,M), N is M + 1.
