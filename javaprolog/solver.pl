a330ccc7342aa08c984b89f5fd6194e400bb472a
Medulli <ju.michelet@gmail.com>
2014-05-18 21:45:15 +0200

    solver print

--- javaprolog/solver.pl
+++ javaprolog/solver.pl
@@ -1,7 +1,11 @@
 %Solver
-getPlan([K,-1,move], Plan) :- Plan=[[pick,K]],list_string([K],LStr),string_concat('I pick up the element at place . . . ',LStr,SuccesStr1),string_concat(SuccesStr1,'.',SuccesStr2),nb_setval(output,SuccesStr2).
-getPlan([-1,K,move], Plan) :- Plan=[[drop,K]],list_string([K],LStr),string_concat('I drop it down at place . . . ',LStr,SuccesStr1),string_concat(SuccesStr1,'.',SuccesStr2),nb_setval(output,SuccesStr2).
-getPlan([K1,K2,move], Plan) :- Plan=[[pick,K1],[drop,K2]],list_string([K1],LStr1),string_concat('I pick up the element at place . . . ',LStr1,SuccesStr1),list_string([K2],LStr2),string_concat(SuccesStr1,' . . . and I drop it down at place . . . ',SuccesStr2),string_concat(SuccesStr2,LStr2,SuccesStr3),string_concat(SuccesStr3,'.',SuccesStr4),nb_setval(output,SuccesStr4).
+list_string([], SuccesStr),nb_setval(output,SuccesStr).
+getPlan([K,-1,move], Plan) :- Plan=[[pick,K]],list_string([K],LStr),string_concat('I pick up the element at place . . . ',LStr,SuccesStr1),string_concat(SuccesStr1,'.',SuccesStr2),
+	nb_getval(output,SuccesStr3),string_concat(SuccesStr2,SuccesStr3,SuccesStr4),nb_setval(output,SuccesStr4).
+getPlan([-1,K,move], Plan) :- Plan=[[drop,K]],list_string([K],LStr),string_concat('I drop it down at place . . . ',LStr,SuccesStr1),string_concat(SuccesStr1,'.',SuccesStr2),
+	nb_getval(output,SuccesStr3),string_concat(SuccesStr2,SuccesStr3,SuccesStr4),nb_setval(output,SuccesStr4).
+getPlan([K1,K2,move], Plan) :- Plan=[[pick,K1],[drop,K2]],list_string([K1],LStr1),string_concat('I pick up the element at place . . . ',LStr1,SuccesStr1),list_string([K2],LStr2),string_concat(SuccesStr1,' . . . and I drop it down at place . . . ',SuccesStr2),string_concat(SuccesStr2,LStr2,SuccesStr3),string_concat(SuccesStr3,'.',SuccesStr4),
+	nb_getval(output,SuccesStr5),string_concat(SuccesStr4,SuccesStr5,SuccesStr6),nb_setval(output,SuccesStr6).
 getPlan([L,where], Plan) :- Plan=[],list_string(L,LStr),string_concat('On place(s) . . . ',LStr,SuccesStr),nb_setval(output,SuccesStr).
 getPlan([L,what], Plan) :- Plan=[],(L = [] ->
 	SuccesStr = 'The list of relevant object(s) is . . .  Nothing !'
